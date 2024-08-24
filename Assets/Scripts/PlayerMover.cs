9245aba25829f3b20f3c3759bdc6bb13695efafa
Mathew Jennings <mathew.percussion@gmail.com>
2024-08-24 13:38:43 -0400

    Init repository with Unity Project
    * Add SampleScene, a square for the Player, and a PlayerMover Script

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    private float speedMultiplier = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 translationVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            translationVector += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            translationVector += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            translationVector += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            translationVector += Vector3.right;
+        }
+        gameObject.transform.position += translationVector.normalized * speedMultiplier;
+    }
+}
