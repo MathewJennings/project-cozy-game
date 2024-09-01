689dfcc10cd1e75e038402154ed76b8e08bf9e0c
Mathew Jennings <mathew.percussion@gmail.com>
2024-08-31 20:13:40 -0400

    Implement Next and Previous Page buttons in the recipe book
    * page state is managed in RecipeBookManager.cs directly

--- Assets/ScriptableObjects/Game State/RecipeBookState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewRecipeBookState", menuName = "Game State/Recipe Book State")]
-public class RecipeBookState : ScriptableObject
-{
-    public int currentPageNumber;
-}
--- Assets/Scripts/RecipeBookManager.cs
+++ Assets/Scripts/RecipeBookManager.cs
@@ -15,23 +15,97 @@ public class RecipeBookManager : MonoBehaviour
     private GameObject rightPage;
 
     [SerializeField]
-    private RecipeBookState recipeBookState;
+    private GameObject turnToNextPageButton;
+
+    [SerializeField]
+    private GameObject turnToPreviousPageButton;
+
+    private int currentPageNumber;
+    private GameObject instantiatedLeftRecipe;
+    private GameObject instantiatedRightRecipe;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentPageNumber = 0;
+        UpdateRenderedPages();
+    }
+
+    public void TurnToNextPage()
+    {
+        currentPageNumber++;
+        UpdateRenderedPages();
+    }
+
+    public void TurnToPreviousPage()
+    {
+        currentPageNumber--;
+        UpdateRenderedPages();
+    }
+
+    private void UpdateRenderedPages()
+    {
+        CleanupCurrentRecipes();
+        UpdateLeftPage();
+        UpdateRightPage();
+        UpdateNextPageButton();
+        UpdatePreviousPageButton();
+    }
+
+    private void CleanupCurrentRecipes()
+    {
+        if (instantiatedLeftRecipe != null)
+        {
+            Destroy(instantiatedLeftRecipe);
+        }
+        if (instantiatedRightRecipe != null)
+        {
+            Destroy(instantiatedRightRecipe);
+        }
+    }
+
+    private void UpdateLeftPage()
+    {
         List<Recipe> unlockedRecipes = recipeBookSO.GetUnlockedRecipes();
-        int indexOfLeftPage = 2 * recipeBookState.currentPageNumber;
-        int indexOfRightPage = 2 * recipeBookState.currentPageNumber + 1;
+        int indexOfLeftPage = 2 * currentPageNumber;
         if (indexOfLeftPage < unlockedRecipes.Count)
         {
             Recipe leftRecipe = unlockedRecipes[indexOfLeftPage];
-            Instantiate(leftRecipe.recipePagePrefab, leftPage.transform);
+            instantiatedLeftRecipe = Instantiate(leftRecipe.recipePagePrefab, leftPage.transform);
         }
+    }
+
+    private void UpdateRightPage()
+    {
+        List<Recipe> unlockedRecipes = recipeBookSO.GetUnlockedRecipes();
+        int indexOfRightPage = 2 * currentPageNumber + 1;
         if (indexOfRightPage < unlockedRecipes.Count)
         {
             Recipe rightRecipe = unlockedRecipes[indexOfRightPage];
-            Instantiate(rightRecipe.recipePagePrefab, rightPage.transform);
+            instantiatedRightRecipe = Instantiate(rightRecipe.recipePagePrefab, rightPage.transform);
+        }
+    }
+
+    private void UpdateNextPageButton()
+    {
+        List<Recipe> unlockedRecipes = recipeBookSO.GetUnlockedRecipes();
+        if (unlockedRecipes.Count >= (currentPageNumber + 1) * 2)
+        {
+            turnToNextPageButton.SetActive(true);
+        } else
+        {
+            turnToNextPageButton.SetActive(false);
+        }
+    }
+
+    private void UpdatePreviousPageButton()
+    {
+        if (currentPageNumber > 0)
+        {
+            turnToPreviousPageButton.SetActive(true);
+        } else
+        {
+            turnToPreviousPageButton.SetActive(false);
         }
     }
 }
