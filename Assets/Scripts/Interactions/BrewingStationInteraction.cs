41d0dfdc603713d9d13d6c673b47a3ff7e63c448
Mathew Jennings <mathew.percussion@gmail.com>
2024-09-02 14:06:21 -0400

    Add an E interaction to make the Brewing Station open up a brewing interface
    * The interface only adds a fader to the screen right now
    * NOTE: using the Brewing Station purposefully doesn't pause the game, for now, at least

---
+++ Assets/Scripts/Interactions/BrewingStationInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrewingStationInteraction : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private GameObject brewingInterface;
+
+    private bool isBrewingInterfaceEnabled;
+
+    void Start()
+    {
+        brewingInterface.SetActive(false);
+    }
+
+    public void Interact(GameObject player)
+    {
+        isBrewingInterfaceEnabled = !isBrewingInterfaceEnabled;
+        brewingInterface.SetActive(isBrewingInterfaceEnabled);
+    }
+}
