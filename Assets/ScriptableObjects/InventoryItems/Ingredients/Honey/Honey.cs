c7f3cd607a3764d4ff68af5ccb501645a59ec645
Mathew Jennings <mathew.percussion@gmail.com>
2024-09-03 20:58:51 -0400

    Update the Green Tea Recipe and replace Sugar with Honey (pending new sprites)

---
+++ Assets/ScriptableObjects/InventoryItems/Ingredients/Honey/Honey.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewHoney", menuName = "Inventory/Ingredients/Honey")]
+public class Honey : Ingredient { }
--- Assets/ScriptableObjects/InventoryItems/Ingredients/Sugar/Sugar.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewSugar", menuName = "Inventory/Ingredients/Sugar")]
-public class Sugar : Ingredient { }
