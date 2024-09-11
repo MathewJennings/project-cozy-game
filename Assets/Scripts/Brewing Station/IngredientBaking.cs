2d2efa6a26705f98e6e3e7a4638a207ad0e90f37
Mathew Jennings <mathew.percussion@gmail.com>
2024-09-10 21:46:49 -0400

    Tweak the color deltas of roasting an baking

--- Assets/Scripts/Brewing Station/IngredientBaking.cs
+++ Assets/Scripts/Brewing Station/IngredientBaking.cs
@@ -32,9 +32,9 @@ public class IngredientBaking : IngredientBarManipulator
     {
         UNDER_TARGET_BAR_COLOR = new Color32(0xE2, 0xBA, 0x04, 0xFF);
         TARGET_BAR_COLOR = new Color32(0x06, 0x70, 0x00, 0xFF);
-        TARGET_INGREDIENT_COLOR = new Color32(0xFC, 0xCD, 0x9E, 0xFF);
+        TARGET_INGREDIENT_COLOR = new Color32(0xDF, 0xC8, 0xAF, 0xFF);
         OVERDONE_BAR_COLOR = new Color32(0xA7, 0x00, 0x00, 0xFF);
-        OVERDONE_INGREDIENT_COLOR = new Color32(0x6A, 0x56, 0x43, 0xFF);
+        OVERDONE_INGREDIENT_COLOR = new Color32(0x9E, 0x5B, 0x54, 0xFF);
         base.Start();
     }
 }
--- Assets/Scripts/Brewing Station/IngredientRoasting.cs
+++ Assets/Scripts/Brewing Station/IngredientRoasting.cs
@@ -18,9 +18,9 @@ public class IngredientRoasting : IngredientBarManipulator
     {
         UNDER_TARGET_BAR_COLOR = new Color32(0xE2, 0xBA, 0x04, 0xFF);
         TARGET_BAR_COLOR = new Color32(0x06, 0x70, 0x00, 0xFF);
-        TARGET_INGREDIENT_COLOR = new Color32(0xFC, 0xCD, 0x9E, 0xFF);
+        TARGET_INGREDIENT_COLOR = new Color32(0xCF, 0xB3, 0x76, 0xFF);
         OVERDONE_BAR_COLOR = new Color32(0xA7, 0x00, 0x00, 0xFF);
-        OVERDONE_INGREDIENT_COLOR = new Color32(0x6A, 0x56, 0x43, 0xFF);
+        OVERDONE_INGREDIENT_COLOR = new Color32(0x89, 0x6E, 0x34, 0xFF);
         base.Start();
     }
 }
